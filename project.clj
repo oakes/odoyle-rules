4978eec42d1cf423c3d3ee96d7264ace0e679c02
oakes <zach@sekao.net>
2020-07-19 04:42:55 -0400

    Change repo and artifact name

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject net.sekao/odoyle "0.1.0-SNAPSHOT"
-  :url "https://github.com/oakes/odoyle"
+(defproject net.sekao/odoyle-rules "0.1.0-SNAPSHOT"
+  :url "https://github.com/oakes/odoyle-rules"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :repositories [["clojars" {:url "https://clojars.org/repo"
