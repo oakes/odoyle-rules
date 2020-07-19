4059dbc2cf65431936e787100d762294ff33a820
oakes <zach@sekao.net>
2020-07-19 17:28:57 -0400

    Add first test

--- test/odoyle/rules_test.clj
+++ test/odoyle/rules_test.clj
@@ -2,3 +2,25 @@
   (:require [clojure.test :refer :all]
             [odoyle.rules :as o]))
 
+(deftest num-of-conditions-not=-num-of-facts
+  (-> (reduce o/add-rule (o/->session)
+        (o/ruleset
+          {::num-conds-and-facts
+           [:what
+            [b ::color "blue"]
+            [y ::left-of z]
+            [a ::color "maize"]
+            [y ::right-of b]
+            [x ::height h]
+            :then
+            (is (= a ::alice))
+            (is (= b ::bob))
+            (is (= y ::yair))
+            (is (= z ::zach))]}))
+      (o/insert ::bob ::color "blue")
+      (o/insert ::yair ::left-of ::zach)
+      (o/insert ::alice ::color "maize")
+      (o/insert ::yair ::right-of ::bob)
+      (o/insert ::xavier ::height 72)
+      (o/insert ::thomas ::height 72)
+      (o/insert ::george ::height 72)))
