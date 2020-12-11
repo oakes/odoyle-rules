2d689e29e84f54ea996838177f2c012849b4d8ff
oakes <zach@sekao.net>
2020-12-11 16:54:17 -0500

    Require benchmark nses dynamically

--- bench.clj
+++ bench.clj
@@ -5,31 +5,31 @@
   (println "Unknown benchmark:" task-name)
   (System/exit 1))
 
-(require '[benchmark.simple])
 (defmethod task "simple"
   [_]
-  (time (benchmark.simple/run 10000)))
+  (require '[benchmark.simple])
+  (time ((resolve 'benchmark.simple/run) 10000)))
 
 (def dungeon-iterations 100)
 
-(require '[benchmark.dungeon-crawler.odoyle])
 (defmethod task "dungeon"
   [_]
-  (time (benchmark.dungeon-crawler.odoyle/run dungeon-iterations)))
+  (require '[benchmark.dungeon-crawler.odoyle])
+  (time ((resolve 'benchmark.dungeon-crawler.odoyle/run) dungeon-iterations)))
 
-(require '[benchmark.dungeon-crawler.clara])
 (defmethod task "dungeon-clara"
   [_]
-  (time (benchmark.dungeon-crawler.clara/run dungeon-iterations)))
+  (require '[benchmark.dungeon-crawler.clara])
+  (time ((resolve 'benchmark.dungeon-crawler.clara/run) dungeon-iterations)))
 
-(require '[benchmark.people.odoyle])
 (defmethod task "people"
   [_]
-  (time (benchmark.people.odoyle/run)))
+  (require '[benchmark.people.odoyle])
+  (time ((resolve 'benchmark.people.odoyle/run))))
 
-(require '[benchmark.people.datascript])
 (defmethod task "people-datascript"
   [_]
-  (time (benchmark.people.datascript/run)))
+  (require '[benchmark.people.datascript])
+  (time ((resolve 'benchmark.people.datascript/run))))
 
 (task *command-line-args*)
